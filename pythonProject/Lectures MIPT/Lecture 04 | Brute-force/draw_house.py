7c8a4599708aab46767c9daabd5e34d3bec28582
priz <priz1920@gmail.com>
2021-03-16 20:30:53 +0300

    Create draw_house.py

---
+++ pythonProject/Lectures MIPT/Lecture 04 | Brute-force/draw_house.py
@@ -0,0 +1,15 @@
+import graphics as gr
+
+def build_house(window, upper_left_corner, width):
+    """ Функция, которая рисует дом"""
+    pass
+
+window = gr.GraphWin("Russian game", 300,300)
+build_house(window, gr.Point(100, 100), 100)
+
+print("Ура! Дом построен!")
+
+
+
+window.getMouse()
+window.close()
