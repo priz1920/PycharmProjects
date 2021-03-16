5d2de733f73365ceb2767f4d93c127b35cfa8a54
priz <priz1920@gmail.com>
2021-03-16 20:32:41 +0300

    Delete matryoshka.py

--- pythonProject/Lectures MIPT/Lecture 7 | Recurcion/matryoshka.py
+++
@@ -1,8 +0,0 @@
-def matryoshka (n):
-    if n == 1:
-        print("Матрешечка")
-    else:
-        print("Верх матрешки n=", n)
-        matryoshka(n-1)
-        print("Низ матрешки n=", n)
-        
