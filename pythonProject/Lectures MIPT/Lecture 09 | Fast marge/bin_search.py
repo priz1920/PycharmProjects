c292ed02b60c76e89e050aa802ff5cf7bb9612e9
priz <priz1920@gmail.com>
2021-03-16 20:33:08 +0300

    Create bin_search.py

---
+++ pythonProject/Lectures MIPT/Lecture 09 | Fast marge/bin_search.py
@@ -0,0 +1,24 @@
+"""Бинарный поск эелемента в массиве"""
+
+def left_bound(A, key):
+    left = -1
+    right = len(A)
+    while right - left > 1:
+        middle = (left + right) // 2
+        if A[middle] < key:
+            left = middle
+        else:
+            right = middle
+    return left
+
+def right_bound(A, key):
+    left = -1
+    right = len(A)
+    while right - left > 1:
+        middle = (left + rirht) // 2
+        if A[middle] <= key:
+            left = middle
+        else:
+            rirht = middle
+    return right
+
